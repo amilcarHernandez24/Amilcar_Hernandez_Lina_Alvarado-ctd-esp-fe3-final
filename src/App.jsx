b314a2175a1ad3c5c2d6e2ce8e0145180eb2a427
Amilcar <amilcarjh24@gmail.com>
2024-09-09 07:53:28 -0300

    Add react route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/favorites" element={<Favs />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
